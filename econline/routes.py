2f571f965bdba47fd4456079471b4c850871bb5a
averagewifiuser <averagewifiuser@gmail.com>
2021-05-08 01:36:10 +0100

    Added a contact us page

--- econline/routes.py
+++ econline/routes.py
@@ -1,6 +1,7 @@
 from econline import app
-from flask import render_template, url_for, session
+from flask import render_template, url_for, session, request, flash
 from datetime import timedelta
+from econline.functions import send_mail
 
 @app.route('/')
 def home():
@@ -13,6 +14,29 @@ def thank_you():
 @app.route('/error')
 def flash_error():
     return render_template('flash-error.html', title="Oh No!")
+
+@app.route('/contact-us', methods=['POST', 'GET'])
+def contact_us():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+        
+        recipient = 'bhjcrelections21@gmail.com'
+        html = message + " My email is: " + email
+        subject = name + " - " + subject
+        
+        send_mail(recipient, subject, html)
+        
+        subject_1 = 'Email Received!'
+        html_1 = 'We have received your email. Thank you for contacting us. \nThis is an automatic email. Please do not reply.'
+        
+        send_mail(email, subject_1, html_1)
+        
+        flash('The email has been sent!', 'success')
+        
+    return render_template('contact-us.html', title='Contact Us')
 
 @app.before_request
 def make_session_permanent():
